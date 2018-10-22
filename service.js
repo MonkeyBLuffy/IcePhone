3b208201ec1bfd38e3777c5a271be6fb875663c6
dongyu <tangchao36@163.com>
2018-10-22 21:29:18 +0800

    整理一下项目

--- service.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-const app = express()
-
-// const api = require('./api')
-// api(app)
-
-app.listen(3000)
-
-console.log('Server running at http://127.0.0.1:3000/');
