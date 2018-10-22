194182c1627f3dcbb44cdab7a569042808ef778e
icephone_1 <lincy1021@163.com>
2018-10-22 21:13:33 +0800

    添加后台

---
+++ service.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const app = express()
+
+// const api = require('./api')
+// api(app)
+
+app.listen(3000)
+
+console.log('Server running at http://127.0.0.1:3000/');
