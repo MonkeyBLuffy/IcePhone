b637ebbdbba9806242772afe35610e9120d3e36c
yusenD <tangchao36@163.com>
2018-10-30 20:18:11 +0800

    改了Schema，添加了一些接口

--- repaireWeb/api.js
+++ repaireWeb/api.js
@@ -11,49 +11,49 @@ module.exports = function (app) {
   });
   // api login
 
-  //维修列表==================================================
-  //获取维修列表
-  app.get('/repaire/app/getRepairedList', function (req, res) {
-    
-    let repaireBean = [];
+  //维修列表================================================================================
 
+  //获取维修列表 //完成
+  app.get('/repair/app/getrepairList', function (req, res) {
+    
+    let repairBean = [];
     const param = {};
     const time = {};
-    if(req.query.centerName!='') param.center_name = req.query.centerName;
-    if(req.query.startTime!='') {
+    if(req.query.centerName!=undefined) param.center_name = req.query.centerName;
+    if(req.query.startTime!=undefined) {
       time.$gte = req.query.startTime;
       param.time = time;
     }
-    if(req.query.endTime!='') {
+    if(req.query.endTime!=undefined) {
       time.$lte = req.query.endTime;
       param.time = time;
     }
-    
-    // repaire
-    const getRepaireBean = new Promise((resolve, reject) => {
-      db.repaire_test.find(
+    console.log(param)
+    // repair
+    const getrepairBean = new Promise((resolve, reject) => {
+      db.repair.find(
         param,
         {__v:0},
         function (err, doc) {
           if (err) {
-            console.log('repaireBean find error!')
-            reject('reject repaireBean')
+            console.log('repairBean find error!')
+            reject('reject repairBean')
           } else {
             if (!doc) {
-              repaireBean = [];
+              repairBean = [];
             } else {
-              repaireBean = doc;
+              repairBean = doc;
             }
-            resolve(repaireBean)
+            resolve(repairBean)
           }
         })
     })
 
-    const p_all = Promise.all([getRepaireBean])
+    const p_all = Promise.all([getrepairBean])
 
     p_all.then((suc) => {
       let data = {
-        "RepaireBean": suc[0]
+        "repairBean": suc[0]
       }
       res.json({ code: 200, msg: '查询成功', data: data })
       return
@@ -65,13 +65,14 @@ module.exports = function (app) {
   })
 
   //删除维修记录
-  app.get('/repaire/app/deleteRepaireRecore', function (req, res) {
+  app.get('/repair/app/deleterepairRecord', function (req, res) {
 
   })
 
-  //修改维修记录
-  app.post('/repaire/app/changeRepaireRecord', function (req, res) {
-    db.repaire_test.findByIdAndUpdate(
+  //修改维修记录--完成
+  app.post('/repair/app/changerepairRecord', function (req, res) {
+    console.log(req.body)
+    db.repair.findByIdAndUpdate(
       req.body._id,
       req.body,
       {upsert:true},
@@ -87,9 +88,9 @@ module.exports = function (app) {
     )
   })
 
-  //增加维修记录
-  app.post('/repaire/app/addRepaireRecord', function (req, res) {
-    db.repaire_test.create(
+  //增加维修记录--完成
+  app.post('/repair/app/addrepairRecord', function (req, res) {
+    db.repair.create(
       req.body,
       function(err,doc){
         if(err){
@@ -105,13 +106,13 @@ module.exports = function (app) {
   })
 
   //联社中心==========================================
-  //获取联社中心列表
-  app.get('/repaire/app/getCenterList', function (req, res) {
+  //获取联社中心列表--完成
+  app.get('/repair/app/getCenterList', function (req, res) {
     let centerBean = [];
 
     // center
     const getCenter = new Promise((resolve, reject) => {
-      db.center_test.find(
+      db.center.find(
         {},
         function (err, doc) {
           if (err) {
@@ -143,25 +144,222 @@ module.exports = function (app) {
     })
 
   })
-  //删除维修记录
-  app.get('/repaire/app/deleteCenterRecore', function (req, res) {
+  //删除中心记录
+  app.get('/repair/app/deleteCenterRecord', function (req, res) {
 
+  })
+  //增加中心记录
+  app.post('/repair/app/addCenterRecord', function (req, res) {
 
   })
 
-  //修改维修记录
-  app.post('/repaire/app/changeCenterRecord', function (req, res) {
+  //设备详细=====================================
+  //查询设备详细
+  app.get('/repair/app/getDeviceList', function (req, res) {
+    let centerBean = [];
 
+    // center
+    const getCenter = new Promise((resolve, reject) => {
+      db.center.find(
+        {},
+        function (err, doc) {
+          if (err) {
+            console.log('center find error!');
+            reject('reject center')
+          } else {
+            if (!doc) {
+              centerBean = [];
+            } else {
+              centerBean = doc;
+            }
+            resolve(centerBean)
+          }
+        })
+    })
+
+    const p_all = Promise.all([getCenter])
+
+    p_all.then((suc) => {
+      let data = {
+        "CenterBean": suc[0]
+      }
+      res.json({ code: 200, msg: '查询成功', data: data })
+      return
+    }).catch((err) => {
+      console.log('err all:' + err)
+      res.json({ code: 600, msg: '查询出错', data: data })
+      return
+    })
+
+  })
+  //增加设备详细
+  app.get('/repair/app/addDevice', function (req, res) {
+
+  })
+  //删除设备详细
+  app.post('/repair/app/deleteDevice', function (req, res) {
 
   })
 
-  //增加维修记录
-  app.post('/repaire/app/addCenterRecord', function (req, res) {
+  //人员管理=======================================
+  //查询人员
+  app.get('/repair/app/getrepairList', function (req, res) {
+    
+    let repairBean = [];
+    const param = {};
+    const time = {};
+    if(req.query.centerName!=undefined) param.center_name = req.query.centerName;
+    if(req.query.startTime!=undefined) {
+      time.$gte = req.query.startTime;
+      param.time = time;
+    }
+    if(req.query.endTime!=undefined) {
+      time.$lte = req.query.endTime;
+      param.time = time;
+    }
+    console.log(param)
+    // repair
+    const getrepairBean = new Promise((resolve, reject) => {
+      db.repair.find(
+        param,
+        {__v:0},
+        function (err, doc) {
+          if (err) {
+            console.log('repairBean find error!')
+            reject('reject repairBean')
+          } else {
+            if (!doc) {
+              repairBean = [];
+            } else {
+              repairBean = doc;
+            }
+            resolve(repairBean)
+          }
+        })
+    })
 
+    const p_all = Promise.all([getrepairBean])
+
+    p_all.then((suc) => {
+      let data = {
+        "repairBean": suc[0]
+      }
+      res.json({ code: 200, msg: '查询成功', data: data })
+      return
+    }).catch((err) => {
+      console.log('err all:' + err)
+      res.json({ code: 600, msg: '查询出错', data: data })
+      return
+    })
+  })
+  //增加人员
+  app.get('/repair/app/addPerson', function (req, res) {
+
+  })
+  //删除人员
+  app.get('/repair/app/deletePerson', function (req, res) {
+
+  })
+  //修改人员
+  app.post('/repair/app/changePerson', function (req, res) {
 
   })
 
+  //维修项目明细====================================
+  //查询项目详细
+  app.get('/repair/app/getProjectList', function (req, res) {
+    let centerBean = [];
 
+    // center
+    const getCenter = new Promise((resolve, reject) => {
+      db.center.find(
+        {},
+        function (err, doc) {
+          if (err) {
+            console.log('center find error!');
+            reject('reject center')
+          } else {
+            if (!doc) {
+              centerBean = [];
+            } else {
+              centerBean = doc;
+            }
+            resolve(centerBean)
+          }
+        })
+    })
+
+    const p_all = Promise.all([getCenter])
+
+    p_all.then((suc) => {
+      let data = {
+        "CenterBean": suc[0]
+      }
+      res.json({ code: 200, msg: '查询成功', data: data })
+      return
+    }).catch((err) => {
+      console.log('err all:' + err)
+      res.json({ code: 600, msg: '查询出错', data: data })
+      return
+    })
+
+  })
+  //增加设备详细
+  app.get('/repair/app/addProject', function (req, res) {
+
+  })
+  //删除设备详细
+  app.post('/repair/app/deleteProject', function (req, res) {
+
+  })
+
+  //维修网点明细====================================
+  //查询网点详细
+  app.get('/repair/app/getSiteList', function (req, res) {
+    let centerBean = [];
+
+    // center
+    const getCenter = new Promise((resolve, reject) => {
+      db.center.find(
+        {},
+        function (err, doc) {
+          if (err) {
+            console.log('center find error!');
+            reject('reject center')
+          } else {
+            if (!doc) {
+              centerBean = [];
+            } else {
+              centerBean = doc;
+            }
+            resolve(centerBean)
+          }
+        })
+    })
+
+    const p_all = Promise.all([getCenter])
+
+    p_all.then((suc) => {
+      let data = {
+        "CenterBean": suc[0]
+      }
+      res.json({ code: 200, msg: '查询成功', data: data })
+      return
+    }).catch((err) => {
+      console.log('err all:' + err)
+      res.json({ code: 600, msg: '查询出错', data: data })
+      return
+    })
+
+  })
+  //增加设备详细
+  app.get('/repair/app/addSite', function (req, res) {
+
+  })
+  //删除设备详细
+  app.post('/repair/app/deleteSite', function (req, res) {
+
+  })
 
   app.get('*', function (req, res) {
     res.end('404')
--- repaireWeb/db.js
+++ repaireWeb/db.js
@@ -7,17 +7,57 @@ const DB_URL = 'mongodb://127.0.0.1:27017/repairdb';
 const centerSchema = new Schema({
     center_name:String
 })
+// 设备详细--模型
+const deviceSchema = new Schema({
+    device_name:String
+})
+// 安装--模型
+const installSchema = new Schema({
+    time:String,
+    center_name:String,
+    site_name:String,
+    site_person:String,
+    install_person:String,
+    install_pro:String,
+    device:String,
+    install_state:String,
+    install_complete:Number,
+    install_cost:String,
+})
+// 人员--模型
+const personSchema = new Schema({
+    nick_name:String,
+    account:String,
+    password:String,
+    limit:Number,
+    manage_center:String,
+})
+// 项目--模型
+const projectSchema = new Schema({
+    project_name:String,
+})
 
-// 维修项目的的数据结构模型
-const repaireSchema = new Schema({
+// 维修记录——模型
+const repairSchema = new Schema({
   time: String,
   site_name: String,
   repair_pro: String,
   repair_person: String,
   site_person: String,
-  center_name: String
+  center_name: String,
+  device:String,
+  fix_state:String,
+  return_fix:Number,
+  fix_cost:String,
+  return_time:String,
+})
+// 网点--模型
+const siteSchema = new Schema({
+    site_name:String,
+    site_property:String,
 })
 
+mongoose.set('useFindAndModify', false)
 mongoose.Promise = global.Promise;
 mongoose.connect(DB_URL);
 const database =  mongoose.connection;
@@ -31,8 +71,8 @@ database.once('open', function(){
 })
 
 const db = {
-  center_test: mongoose.model('center_test', centerSchema),
-  repaire_test: mongoose.model('repaire_test', repaireSchema),
+  center: mongoose.model('centers', centerSchema),
+  repair: mongoose.model('repairs', repairSchema),
 }
 
 module.exports = db
